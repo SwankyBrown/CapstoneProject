f79ba68eff50b04270635c1cfc6fb23d3a21e8e3
Sheldon Christiansen <bigbrownguy724@gmail.com>
2023-05-24 12:27:41 -0600

    HTML done

---
+++ server/server.js
@@ -0,0 +1,28 @@
+// const express = require("express")
+// const path = require("path")
+// const app = express()
+
+// app.use (express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// app.listen(4000, () => {
+//     console.log("we are up on 4000");
+// })
